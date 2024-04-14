808d8a4edd735ece6d66860a97ce2a0cb6404d01
sushant babu prasai <sushantprasaii7@gmail.com>
2024-04-14 14:32:15 +0545

    week 1 task added

---
+++ week1/Hello.java
@@ -0,0 +1,22 @@
+package week1;
+
+public class Hello {
+    public static void main(String[] args) {
+        // System.out.println();
+        // System.out.print("This is my first program I am on module ST4003CEM");
+        // System.out.print("This is my first program I am on module ST4003CEM");
+        // System.out.print(
+        // "A \" quoted \" String is \n 'much' better if you learn the rules of \"escape
+        // sequences.\"\n Also, \"\" represents an empty\n String. Don't forget: use
+        // \"\n instead of \"!\n'' is not the same as \n");
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+
+    }
+}
