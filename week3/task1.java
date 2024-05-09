1167d96c7d179411c4b2863b7cbd4976653021da
sushant babu prasai <sushantprasaii7@gmail.com>
2024-05-09 23:09:47 +0545

    Feat: week 3 question

---
+++ week3/task1.java
@@ -0,0 +1,13 @@
+package week3;
+
+public class task1 {
+    public static void main(String[] args) {
+        int number = 123;
+        if (number % 5 == 0 && number % 11 == 0) {
+            System.out.println("Divisible by 5 and 11");
+        } else {
+            System.out.println("Not Divisible by 5 and 11");
+        }
+    }
+
+}
