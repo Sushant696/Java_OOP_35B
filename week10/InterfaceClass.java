e6a156ba055dba147c4d78759e10b83c600603f3
sushant696 <sushantprasaii7@gmail.com>
2024-06-21 15:06:33 +0545

    feat: interface class

---
+++ week10/InterfaceClass.java
@@ -0,0 +1,30 @@
+package week10;
+
+public class InterfaceClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+
+}
+
+/*
+ * Task
+ * Make an interface Media
+ * make the following functions
+ * void play()
+ * void pause()
+ * void next()
+ * void previous()
+ * String nowPlaying()
+ * 
+ * Make an interface DigitalMedia extending Media
+ * make the following functions
+ * double size()
+ * double remaining(double current)
+ * 
+ * Make an interface OnlineAsset
+ * make the following functions
+ * void download()
+ * 
+ * Make a class Spotify and implement DigitalMedia and OnlineAsset
+ */
