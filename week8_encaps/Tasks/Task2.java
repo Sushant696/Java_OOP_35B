05149e490b5adf927a63b37f493c5a51d590ec83
sushant696 <sushantprasaii7@gmail.com>
2024-06-28 15:03:22 +0545

    feat:exceptions handling

---
+++ week8_encaps/Tasks/Task2.java
@@ -0,0 +1,12 @@
+package week8_encaps.Tasks;
+
+public class Task2 {
+
+}
+
+// You are developing a restaurant management system and want to create a Menu
+// class. The Menu class should encapsulate a list of MenuItem objects.
+// Implement the Menu class with private instance variables for the menu items
+// and provide public methods to add and remove items from the menu. Ensure that
+// the list of menu items cannot be directly accessed or modified from outside
+// the class.
